a379dab7343fb89fbac49d61d0bad9aeec0a4b8b
ZHENG-YEN <paec55612@gmail.com>
2026-03-17 12:54:10 +0800

    Update check_stock_debug.py

--- check_stock_debug.py
+++ check_stock_debug.py
@@ -14,13 +14,13 @@ pd.set_option('display.max_colwidth', None)
 
 ticker = yf.Ticker("0050.TW")
 df = ticker.history(period=f"10d", interval="1d")
-print(f"history daily:\n {df}")
+print(f"\nhistory daily:\n {df}")
 
     
     
 # 抓取最近 1 分鐘的資料 (這會回傳包含最新一筆的時間)
 df_now = ticker.history(period="1d", interval="1m")
-print(f"history min:\n {df_now}")
+print(f"\nhistory min:\n {df_now}")
 
 if not df_now.empty:
     latest_time = df_now.index[-1]
@@ -34,12 +34,12 @@ print("\n\n")
 symbol = "0050.TW" 
 # 1. 下載日 K 資料 (例如最近 5 天)
 df_daily = yf.download(symbol, period="5d", interval="1d")
-print(f"daily download:\n {df_daily}")
+print(f"\ndaily download:\n {df_daily}")
 
 # 2. 下載最近 1 天的分鐘線 (用來取得「最後成交時間」)
 # 因為只需要最後一筆時間，所以 period="1d", interval="1m" 負擔最小
 df_min = yf.download(symbol, period="1d", interval="1m")
-print(f"min download:\n\n {df_min}")
+print(f"\nmin download:\n\n {df_min}")
 
 # 取得 1m 資料的最後一個索引 (即為該股目前的最新成交時間)
 if not df_min.empty:
